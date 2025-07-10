f81dcdbf01a3c5f426d3e8e55ab784b9e0302ab2
Uttam <uttamrohit4545@gmail.com>
2025-07-10 23:26:01 +0600

    feat:navbar implement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,7 @@ const Navbar = () => {
           {itemCount > 0 && (
             <span class="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-1">
                 {itemCount}
+                
             </span>
           )}
         </Link>
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -30,6 +30,7 @@ export const CartProvider = ({children}) => {
             {children}
         </CartContext.Provider>
         
+        
         </>
     );
 };
