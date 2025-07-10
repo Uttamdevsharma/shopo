609c8a8198ad43750a9c746355f5e37d99e916f7
Uttam <uttamrohit4545@gmail.com>
2025-07-11 03:03:14 +0600

    feat : Cart context api build

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -6,12 +6,11 @@ export const CartProvider = ({children}) => {
     const [cartItems,setCartItems] = useState([]);
 
     const addToCart = (product) => {
-        setCartItems((prev) => {
+        setCartItems( (prev) => {
             const exists = prev.find((item) => item.id === product.id);
             if(exists) {
                 return prev.map((item) => 
-                    item.id === product.id ? {...item ,quantity: item.quantity+1} : item
-
+                    item.id === product.id ? { ...item ,quantity: item.quantity+1} : item
                 );
             }
             return [...prev , { ...product,quantity: 1}];
@@ -29,7 +28,7 @@ export const CartProvider = ({children}) => {
         <CartContext.Provider value={{cartItems,addToCart,removeFromCart}} >
             {children}
         </CartContext.Provider>
-        
+
         
         </>
     );
